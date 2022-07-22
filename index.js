9aaa26275f34bc25a60ef8a10243368119f0caf7
Dylan Hays <twentygx@outlook.com>
2022-07-22 18:29:20 -0500

    commit tree abe59ca24734672cd769e25c2c70fd6a22217466

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+function moveDodgerLeft() {
+    dodger.style.left = `${getDodgerHPos() - 1}px`;
+}
+
+function moveDodgerRight() {
+    dodger.style.left = `${getDodgerHPos() + 1}px`;
+}
+
+function getDodgerHPos() {
+    const dodgerL = dodger.style.left.replace("px", "");
+    return parseInt(dodgerL, 10);
+}
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft")
+        moveDodgerLeft();
+    else if (event.key === "ArrowRight")
+        moveDodgerRight();
+});
